20a9e383167d5481a0b03117883ac89c3b3ee092
KhanRana <rana_khan@outlook.com>
2022-11-19 16:46:36 +0000

    Add employee class

---
+++ employee.py
@@ -0,0 +1,48 @@
+class Employee:
+    """
+    Employee class
+    """
+    no_of_employees = 0
+    raise_pay = 1.04
+
+    def __init__(self, first, last, pay):
+        """
+        Initialize the employee class
+        """
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.name = first + ' ' + last
+        Employee.no_of_employees += 1
+
+    def email(self):
+        """
+        Create employee's email address
+        """
+        return f"{self.first}.{self.last}@company.com"
+
+    def full_name(self):
+        """
+        Get employee's full name
+        """
+        return self.first.upper() + ' ' + self.last.upper()
+
+    def apply_raise(self):
+        """
+        Apply pay raise to an employee
+        """
+        self.pay = self.raise_pay * self.pay
+
+    # create a class method to increase the raise amount
+    @classmethod
+    def pay_raise(cls, amt):
+        """
+        Increase the raise amount
+        """
+        cls.raise_pay = amt
+
+    def __str__(self):
+        """
+        modify str to return the employee and employee's email 
+        """
+        return f"{self.full_name()} - {self.email()}"
